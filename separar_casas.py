8031b14f9d0318cc91a3ace62fcc9c0b17a2ca0a
heigon <heigon@ciandt.com>
2023-04-14 17:47:55 -0300

    Corners

--- separar_casas.py
+++ separar_casas.py
@@ -1,7 +1,7 @@
 import cv2
 import numpy as np
 
-imagem = cv2.imread('imagens\i8457.png', cv2.IMREAD_COLOR)
+imagem = cv2.imread('i3.png', cv2.IMREAD_COLOR)
 
 bordas = cv2.Canny(imagem, 100, 150, apertureSize=3)
 
@@ -42,13 +42,13 @@ for linha in arr_d1:
     b = np.sin(theta)
     x0 = a * rho
     y0 = b * rho
-    x1 = int(x0 + 10000 * (-b))
-    y1 = int(y0 + 10000 * (a))
-    x2 = int(x0 - 10000 * (-b))
-    y2 = int(y0 - 10000 * (a))
-    cv2.line(images_linhasd1, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    x1 = int(x0 + 500 * (-b))
+    y1 = int(y0 + 500 * (a))
+    x2 = int(x0 - 500 * (-b))
+    y2 = int(y0 - 500 * (a))
+    cv2.line(imagem, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-cv2.imshow('Imagem com linhas detectadas', images_linhasd1)
+cv2.imshow('Imagem com linhas detectadas', imagem)
 cv2.waitKey(0)
 
 images_linhasd2 = imagem.copy()
@@ -59,50 +59,94 @@ for linha in arr_d2:
     b = np.sin(theta)
     x0 = a * rho
     y0 = b * rho
-    x1 = int(x0 + 10000 * (-b))
-    y1 = int(y0 + 10000 * (a))
-    x2 = int(x0 - 10000 * (-b))
-    y2 = int(y0 - 10000 * (a))
-    cv2.line(images_linhasd2, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    x1 = int(x0 + 500 * (-b))
+    y1 = int(y0 + 500 * (a))
+    x2 = int(x0 - 500 * (-b))
+    y2 = int(y0 - 500 * (a))
+    cv2.line(imagem, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-cv2.imshow('Imagem com linhas detectadas', images_linhasd2)
+cv2.imshow('Imagem com linhas detectadas', imagem)
 cv2.waitKey(0)
 
+import numpy as np
+
+def calcular_ponto_intersecao(rho1, theta1, rho2, theta2):
+    # Converter coordenadas polares para coordenadas cartesianas
+    x1 = rho1 * np.cos(theta1)
+    y1 = rho1 * np.sin(theta1)
+    x2 = rho2 * np.cos(theta2)
+    y2 = rho2 * np.sin(theta2)
+
+    # Escrever as equações paramétricas das retas em coordenadas cartesianas
+    a1 = x1 - x2
+    b1 = -x1
+    c1 = y1 - y2
+    d1 = -y1
+    a2 = x2 - x1
+    b2 = -x2
+    c2 = y2 - y1
+    d2 = -y2
+
+    # Calcular o valor de t onde as retas se intersectam
+    t = (d1 - d2) / (c1 - c2)
+
+    # Calcular os valores correspondentes de x e y no ponto de interseção
+    x_intersecao = a1 * t + b1
+    y_intersecao = c1 * t + d1
+
+    return x_intersecao, y_intersecao
 
 
-def calcular_ponto_intersecao(l1, l2):
-    # Extrai os valores de rho e theta das equações paramétricas das retas em l1 e l2
-    rho1 = l1[0]
-    theta1 = l1[1]
 
-    rho2 = l1[0]
-    theta2 = l2[1]
+def separar_quadrados(arr_d1, arr_d2, img_original):
+    # Separa as coordenadas das retas em pares (rho, theta)
+    coordenadas = []
+    for linha1 in range(len(arr_d1) - 1):
+        for linha2 in range(len(arr_d2) - 1):
+
+            coordenadas.append((linha1[0], linha1[1], linha2[0], linha2[1]))
+
+    # Cria uma imagem em branco para desenhar os quadrados
+    img_quadrados = np.zeros_like(img_original)
+
+    # Loop para desenhar os quadrados
+    for coord in coordenadas:
+        rho1, theta1, rho2, theta2 = coord
+        a1, b1 = np.cos(theta1), np.sin(theta1)
+        a2, b2 = np.cos(theta2), np.sin(theta2)
+        x01, y01 = a1 * rho1, b1 * rho1
+        x02, y02 = a2 * rho2, b2 * rho2
+        x11, y11 = int(x01 * (-b1)), int(y01 * (a1))
+        x12, y12 = int(x01 * (-b1)), int(y01 * (a1))
+        x21, y21 = int(x02 * (-b2)), int(y02 * (a2))
+        x22, y22 = int(x02 * (-b2)), int(y02 * (a2))
+
+        print(x11, y11)
+        print(x22, y22)
+        # Desenha os quadrados na imagem em branco
+        cv2.rectangle(img_quadrados, (x11, y11), (x22, y22), (0, 255, 0), 2)
     
-    # Calcula o seno e cosseno dos ângulos theta1 e theta2
-    cos_theta1 = np.cos(theta1)
-    sin_theta1 = np.sin(theta1)
-    cos_theta2 = np.cos(theta2)
-    sin_theta2 = np.sin(theta2)
-    
-    # Calcula os coeficientes a e b do sistema de equações lineares
-    a = np.array([[cos_theta1, sin_theta1],
-                  [cos_theta2, sin_theta2]])
-    b = np.array([rho1, rho2])
-    
-    # Resolve o sistema de equações lineares para obter os valores de x e y
-    x, y = np.linalg.solve(a, b)
-    
-    # Retorna o ponto de interseção como uma tupla (x, y)
-    return x, y
+    cv2.imshow('Imagem com linhas detectadas', img_quadrados)
+    cv2.waitKey(0)
 
+    # Recorta os quadrados da imagem original
+    quadrados = []
+    for coord in coordenadas:
+        rho1, theta1, rho2, theta2 = coord
+        a1, b1 = np.cos(theta1), np.sin(theta1)
+        a2, b2 = np.cos(theta2), np.sin(theta2)
+        x01, y01 = a1 * rho1, b1 * rho1
+        x02, y02 = a2 * rho2, b2 * rho2
+        x11, y11 = int(x01 * (-b1)), int(y01 * (a1))
+        x22, y22 = int(x02 * (-b2)), int(y02 * (a2))
+        quadrado = img_original[y11:y22, x11:x22]
+        quadrados.append(quadrado)
 
-print(calcular_ponto_intersecao(arr_d1[0], arr_d2[0]))
-# # Criação das imagens separadas para cada casa
-# for i in range(len(linhas_casas)):
-#     for j in range(i+1, len(linhas_casas)):
-#         ponto_intersecao = obter_ponto_intersecao(linhas_casas[i], linhas_casas[j])
-#         x, y = ponto_intersecao
-#         if 0 <= x < imagem.shape[1] and 0 <= y < imagem.shape[0]:
-#             imagem_casa = imagem[y-300:y+300, x-300:x+300]
-#             if imagem_casa.shape[0] > 0 and imagem_casa.shape[1] > 0:
-#                 cv2.imwrite(f'casa_{i+1}_{j+1}.png', imagem_casa)
+    return quadrados
+
+quadrados = separar_quadrados(arr_d1, arr_d2, imagem)
+
+print(quadrados[0].shape)
+
+cv2.imshow('Imagem com linhas detectadas', quadrados[0])
+cv2.waitKey(0)
--- teste.py
+++ teste.py
@@ -1,28 +1,101 @@
-import torch
-import torch.nn as nn
-import torch.optim as optim
+import cv2
+import numpy as np
 
-# Dados de exemplo
-num_classes = 3
-input_size = 5
-batch_size = 2
+# Load the image
+imagem = cv2.imread('i3.png', cv2.IMREAD_COLOR)
 
-# Gerando dados de exemplo
-inputs = torch.randn(batch_size, input_size)  # Entradas do modelo
-labels = torch.empty(batch_size, dtype=torch.long).random_(num_classes)  # Rótulos verdadeiros
+# Perform edge detection
+bordas = cv2.Canny(imagem, 100, 150, apertureSize=3)
 
-# Instanciando o modelo e a função de perda
-model = nn.Linear(input_size, num_classes)  # Modelo de exemplo com uma camada linear
-loss_fn = nn.CrossEntropyLoss()  # Função de perda CrossEntropyLoss
+# Detect lines using Hough transform
+linhas = cv2.HoughLines(bordas, 1, np.pi/180, 155)
 
-# Calculando a perda
-outputs = model(inputs)  # Passando as entradas pelo modelo para obter as previsões
-loss = loss_fn(outputs, labels)  # Calculando a perda com base nas previsões e rótulos verdadeiros
+# Group similar lines
+linhas_agrupadas = []
+for linha in linhas:
+    rho, theta = linha[0]
+    agrupada = False
+    for linha_agrupada in linhas_agrupadas:
+        rho_agrupada, theta_agrupada = linha_agrupada[0]
+        if abs(rho - rho_agrupada) < 30 and abs(theta - theta_agrupada) < np.pi/18:
+            agrupada = True
+            break
+    if not agrupada:
+        linhas_agrupadas.append(linha)
 
-# Backpropagation e otimização
-optimizer = optim.SGD(model.parameters(), lr=0.1)  # Otimizador de exemplo (Gradiente Descendente Estocástico)
-optimizer.zero_grad()  # Zerando os gradientes dos parâmetros do modelo
-loss.backward()  # Backpropagation para calcular os gradientes
-optimizer.step()  # Atualizando os parâmetros do modelo com base nos gradientes
+max_rho1 = None
+max_rho2 = None
+min_rho1 = None
+min_rho2 = None
 
-print('Perda:', loss.item())
+for linha in linhas_agrupadas:
+    rho, theta = linha[0]
+    if theta > 1.2:
+
+        if max_rho1 == None:
+            max_rho1 = rho
+        elif rho > max_rho1:
+            max_rho1 = rho
+        
+        if min_rho1 == None:
+            min_rho1 = rho
+        elif rho < min_rho1:
+            min_rho1 = rho
+    else:
+
+        if max_rho2 == None:
+            max_rho2 = rho
+        elif rho > max_rho2:
+            max_rho2 = rho
+        
+        if min_rho2 == None:
+            min_rho2 = rho
+        elif rho < min_rho2:
+            min_rho2 = rho
+
+lines_squares = []
+
+for linha in linhas_agrupadas:
+    rho,theta = linha[0]
+
+    if not (rho == max_rho2 or rho == max_rho1 or rho == min_rho1 or rho == min_rho2):
+        lines_squares.append(linha)
+
+# Extract line coordinates
+line_points = []
+for line in lines_squares:
+    rho, theta = line[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+    line_points.append([(x1, y1), (x2, y2)])
+
+# Find intersection points of lines
+corner_points = []
+for i in range(len(line_points)):
+    for j in range(i+1, len(line_points)):
+        x1, y1 = line_points[i][0]
+        x2, y2 = line_points[i][1]
+        x3, y3 = line_points[j][0]
+        x4, y4 = line_points[j][1]
+        denominator = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
+        if denominator != 0:
+            intersection_x = int(((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4)) / denominator)
+            intersection_y = int(((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4)) / denominator)
+            corner_points.append((intersection_x, intersection_y))
+
+# Draw circles around the corners
+for point in corner_points:
+    x, y = point
+    cv2.circle(imagem, (x, y), 5, (255, 0, 0), 2)
+
+# Display the imagem with corners and circles
+cv2.imshow('Corners and Circles', imagem)
+cv2.imwrite('corners.png', imagem)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
