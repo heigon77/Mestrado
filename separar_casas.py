b3833f3230c326252c42e0140a739803008139e2
heigon <heigon@ciandt.com>
2023-04-15 14:32:05 -0300

    Separa imagens

--- separar_casas.py
+++ separar_casas.py
@@ -1,152 +1,111 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-imagem = cv2.imread('i3.png', cv2.IMREAD_COLOR)
 
-bordas = cv2.Canny(imagem, 100, 150, apertureSize=3)
+imagem = cv.imread('i3.png', cv.IMREAD_COLOR)
 
-linhas = cv2.HoughLines(bordas, 1, np.pi/180, 155)
+cv.imshow('Original', imagem)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+bordas = cv.Canny(imagem, 100, 150, apertureSize=3)
+
+cv.imshow('Canny', bordas)
+cv.waitKey(0)
+cv.destroyAllWindows()
+
+
+linhas = cv.HoughLines(bordas, 1, np.pi/180, 155)
+
+cv.imshow('Hough', linhas)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
 linhas_agrupadas = []
 for linha in linhas:
 
     rho, theta = linha[0]
-    agrupada = False
 
+    agrupada = False
     for linha_agrupada in linhas_agrupadas:
         rho_agrupada, theta_agrupada = linha_agrupada[0]
-        
         if abs(rho - rho_agrupada) < 30 and abs(theta - theta_agrupada) < np.pi/18:
             agrupada = True
             break
-
+        
     if not agrupada:
         linhas_agrupadas.append(linha)
 
-array_d = np.array(linhas_agrupadas)
+max_rho1 = None
+max_rho2 = None
+min_rho1 = None
+min_rho2 = None
 
-arr_d1 = array_d[array_d[:,:,1] > 1.2]
-arr_d2 = array_d[array_d[:,:,1] <= 1.2]
+for linha in linhas_agrupadas:
+    rho, theta = linha[0]
+    if theta > 1.2:
 
-arr_d1 = arr_d1[arr_d1[:,0].argsort()]
-arr_d2 = arr_d2[arr_d2[:,0].argsort()]
+        if max_rho1 == None:
+            max_rho1 = rho
+        elif rho > max_rho1:
+            max_rho1 = rho
+        
+        if min_rho1 == None:
+            min_rho1 = rho
+        elif rho < min_rho1:
+            min_rho1 = rho
+    else:
 
-arr_d1 = arr_d1[1:-1,:]
-arr_d2 = arr_d2[1:-1,:]
+        if max_rho2 == None:
+            max_rho2 = rho
+        elif rho > max_rho2:
+            max_rho2 = rho
+        
+        if min_rho2 == None:
+            min_rho2 = rho
+        elif rho < min_rho2:
+            min_rho2 = rho
 
-images_linhasd1 = imagem.copy()
-for linha in arr_d1:
-    rho = linha[0]
-    theta = linha[1]
+lines_squares = []
+
+for linha in linhas_agrupadas:
+    rho,theta = linha[0]
+
+    if not (rho == max_rho2 or rho == max_rho1 or rho == min_rho1 or rho == min_rho2):
+        lines_squares.append(linha)
+
+line_points = []
+for line in lines_squares:
+    rho, theta = line[0]
     a = np.cos(theta)
     b = np.sin(theta)
     x0 = a * rho
     y0 = b * rho
-    x1 = int(x0 + 500 * (-b))
-    y1 = int(y0 + 500 * (a))
-    x2 = int(x0 - 500 * (-b))
-    y2 = int(y0 - 500 * (a))
-    cv2.line(imagem, (x1, y1), (x2, y2), (0, 0, 255), 2)
+    x1 = int(x0 + 1000 * (-b))
+    y1 = int(y0 + 1000 * (a))
+    x2 = int(x0 - 1000 * (-b))
+    y2 = int(y0 - 1000 * (a))
+    line_points.append([(x1, y1), (x2, y2)])
 
-cv2.imshow('Imagem com linhas detectadas', imagem)
-cv2.waitKey(0)
+corner_points = []
+for i in range(len(line_points)):
+    for j in range(i+1, len(line_points)):
+        x1, y1 = line_points[i][0]
+        x2, y2 = line_points[i][1]
+        x3, y3 = line_points[j][0]
+        x4, y4 = line_points[j][1]
+        denominator = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
+        if denominator != 0:
+            intersection_x = int(((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4)) / denominator)
+            intersection_y = int(((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4)) / denominator)
+            corner_points.append((intersection_x, intersection_y))
 
-images_linhasd2 = imagem.copy()
-for linha in arr_d2:
-    rho = linha[0]
-    theta = linha[1]
-    a = np.cos(theta)
-    b = np.sin(theta)
-    x0 = a * rho
-    y0 = b * rho
-    x1 = int(x0 + 500 * (-b))
-    y1 = int(y0 + 500 * (a))
-    x2 = int(x0 - 500 * (-b))
-    y2 = int(y0 - 500 * (a))
-    cv2.line(imagem, (x1, y1), (x2, y2), (0, 0, 255), 2)
+for point in corner_points:
+    x, y = point
+    cv.circle(imagem, (x, y), 5, (255, 0, 0), 2)
 
-cv2.imshow('Imagem com linhas detectadas', imagem)
-cv2.waitKey(0)
-
-import numpy as np
-
-def calcular_ponto_intersecao(rho1, theta1, rho2, theta2):
-    # Converter coordenadas polares para coordenadas cartesianas
-    x1 = rho1 * np.cos(theta1)
-    y1 = rho1 * np.sin(theta1)
-    x2 = rho2 * np.cos(theta2)
-    y2 = rho2 * np.sin(theta2)
-
-    # Escrever as equações paramétricas das retas em coordenadas cartesianas
-    a1 = x1 - x2
-    b1 = -x1
-    c1 = y1 - y2
-    d1 = -y1
-    a2 = x2 - x1
-    b2 = -x2
-    c2 = y2 - y1
-    d2 = -y2
-
-    # Calcular o valor de t onde as retas se intersectam
-    t = (d1 - d2) / (c1 - c2)
-
-    # Calcular os valores correspondentes de x e y no ponto de interseção
-    x_intersecao = a1 * t + b1
-    y_intersecao = c1 * t + d1
-
-    return x_intersecao, y_intersecao
-
-
-
-def separar_quadrados(arr_d1, arr_d2, img_original):
-    # Separa as coordenadas das retas em pares (rho, theta)
-    coordenadas = []
-    for linha1 in range(len(arr_d1) - 1):
-        for linha2 in range(len(arr_d2) - 1):
-
-            coordenadas.append((linha1[0], linha1[1], linha2[0], linha2[1]))
-
-    # Cria uma imagem em branco para desenhar os quadrados
-    img_quadrados = np.zeros_like(img_original)
-
-    # Loop para desenhar os quadrados
-    for coord in coordenadas:
-        rho1, theta1, rho2, theta2 = coord
-        a1, b1 = np.cos(theta1), np.sin(theta1)
-        a2, b2 = np.cos(theta2), np.sin(theta2)
-        x01, y01 = a1 * rho1, b1 * rho1
-        x02, y02 = a2 * rho2, b2 * rho2
-        x11, y11 = int(x01 * (-b1)), int(y01 * (a1))
-        x12, y12 = int(x01 * (-b1)), int(y01 * (a1))
-        x21, y21 = int(x02 * (-b2)), int(y02 * (a2))
-        x22, y22 = int(x02 * (-b2)), int(y02 * (a2))
-
-        print(x11, y11)
-        print(x22, y22)
-        # Desenha os quadrados na imagem em branco
-        cv2.rectangle(img_quadrados, (x11, y11), (x22, y22), (0, 255, 0), 2)
-    
-    cv2.imshow('Imagem com linhas detectadas', img_quadrados)
-    cv2.waitKey(0)
-
-    # Recorta os quadrados da imagem original
-    quadrados = []
-    for coord in coordenadas:
-        rho1, theta1, rho2, theta2 = coord
-        a1, b1 = np.cos(theta1), np.sin(theta1)
-        a2, b2 = np.cos(theta2), np.sin(theta2)
-        x01, y01 = a1 * rho1, b1 * rho1
-        x02, y02 = a2 * rho2, b2 * rho2
-        x11, y11 = int(x01 * (-b1)), int(y01 * (a1))
-        x22, y22 = int(x02 * (-b2)), int(y02 * (a2))
-        quadrado = img_original[y11:y22, x11:x22]
-        quadrados.append(quadrado)
-
-    return quadrados
-
-quadrados = separar_quadrados(arr_d1, arr_d2, imagem)
-
-print(quadrados[0].shape)
-
-cv2.imshow('Imagem com linhas detectadas', quadrados[0])
-cv2.waitKey(0)
+cv.imshow('Corners and Circles', imagem)
+cv.imwrite('corners.png', imagem)
+cv.waitKey(0)
+cv.destroyAllWindows()
--- teste.py
+++ teste.py
@@ -1,25 +1,52 @@
-import cv2
+import cv2 as cv
 import numpy as np
 
-# Load the image
-imagem = cv2.imread('i3.png', cv2.IMREAD_COLOR)
+imagem = cv.imread('i3.png', cv.IMREAD_COLOR)
 
-# Perform edge detection
-bordas = cv2.Canny(imagem, 100, 150, apertureSize=3)
+cv.imshow('Original', imagem)
+cv.waitKey(0)
+cv.destroyAllWindows()
 
-# Detect lines using Hough transform
-linhas = cv2.HoughLines(bordas, 1, np.pi/180, 155)
+bordas = cv.Canny(imagem, 100, 150, apertureSize=3)
 
-# Group similar lines
-linhas_agrupadas = []
+# cv.imshow('Canny', bordas)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+linhas = cv.HoughLines(bordas, 1, np.pi/180, 160)
+
+images_linhas = imagem.copy()
 for linha in linhas:
     rho, theta = linha[0]
+    a = np.cos(theta)
+    b = np.sin(theta)
+    x0 = a * rho
+    y0 = b * rho
+    x1 = int(x0 + 10000 * (-b))
+    y1 = int(y0 + 10000 * (a))
+    x2 = int(x0 - 10000 * (-b))
+    y2 = int(y0 - 10000 * (a))
+    cv.line(images_linhas, (x1, y1), (x2, y2), (0, 0, 255), 2)
+
+# cv.imshow('Hough', images_linhas)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+
+linhas_agrupadas = []
+for linha in linhas:
+
+    rho, theta = linha[0]
     agrupada = False
-    for linha_agrupada in linhas_agrupadas:
+
+    for id,linha_agrupada in enumerate(linhas_agrupadas):
+
         rho_agrupada, theta_agrupada = linha_agrupada[0]
+
         if abs(rho - rho_agrupada) < 30 and abs(theta - theta_agrupada) < np.pi/18:
             agrupada = True
             break
+
     if not agrupada:
         linhas_agrupadas.append(linha)
 
@@ -30,7 +57,7 @@ min_rho2 = None
 
 for linha in linhas_agrupadas:
     rho, theta = linha[0]
-    if theta > 1.2:
+    if theta > 1:
 
         if max_rho1 == None:
             max_rho1 = rho
@@ -53,49 +80,130 @@ for linha in linhas_agrupadas:
         elif rho < min_rho2:
             min_rho2 = rho
 
-lines_squares = []
+linhas_casas = []
 
 for linha in linhas_agrupadas:
     rho,theta = linha[0]
 
     if not (rho == max_rho2 or rho == max_rho1 or rho == min_rho1 or rho == min_rho2):
-        lines_squares.append(linha)
+        linhas_casas.append(linha)
 
-# Extract line coordinates
-line_points = []
-for line in lines_squares:
-    rho, theta = line[0]
+images_linhas = imagem.copy()
+linhas_pontos = []
+for linha in linhas_casas:
+    rho, theta = linha[0]
     a = np.cos(theta)
     b = np.sin(theta)
     x0 = a * rho
     y0 = b * rho
-    x1 = int(x0 + 1000 * (-b))
-    y1 = int(y0 + 1000 * (a))
-    x2 = int(x0 - 1000 * (-b))
-    y2 = int(y0 - 1000 * (a))
-    line_points.append([(x1, y1), (x2, y2)])
 
-# Find intersection points of lines
-corner_points = []
-for i in range(len(line_points)):
-    for j in range(i+1, len(line_points)):
-        x1, y1 = line_points[i][0]
-        x2, y2 = line_points[i][1]
-        x3, y3 = line_points[j][0]
-        x4, y4 = line_points[j][1]
-        denominator = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
-        if denominator != 0:
-            intersection_x = int(((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4)) / denominator)
-            intersection_y = int(((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4)) / denominator)
-            corner_points.append((intersection_x, intersection_y))
+    x1 = int(x0 + 10000 * (-b))
+    y1 = int(y0 + 10000 * (a))
+    x2 = int(x0 - 10000 * (-b))
+    y2 = int(y0 - 10000 * (a))
 
-# Draw circles around the corners
-for point in corner_points:
-    x, y = point
-    cv2.circle(imagem, (x, y), 5, (255, 0, 0), 2)
+    cv.line(images_linhas, (x1, y1), (x2, y2), (0, 0, 255), 2)
 
-# Display the imagem with corners and circles
-cv2.imshow('Corners and Circles', imagem)
-cv2.imwrite('corners.png', imagem)
-cv2.waitKey(0)
-cv2.destroyAllWindows()
+    linhas_pontos.append([(x1, y1), (x2, y2), theta])
+
+# cv.imshow('Hough', images_linhas)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+pontos = []
+pontos_por_linhas = []
+for i in range(len(linhas_pontos)):
+    pontos_dessa_linha = []
+
+    for j in range(len(linhas_pontos)):
+
+        x1, y1 = linhas_pontos[i][0]
+        x2, y2 = linhas_pontos[i][1]
+        theta = linhas_pontos[i][2]
+
+        x3, y3 = linhas_pontos[j][0]
+        x4, y4 = linhas_pontos[j][1]
+
+        det = (x1-x2)*(y3-y4) - (y1-y2)*(x3-x4)
+
+        if det != 0:
+
+            inter_x = int(((x1*y2-y1*x2)*(x3-x4)-(x1-x2)*(x3*y4-y3*x4)) / det)
+            inter_y = int(((x1*y2-y1*x2)*(y3-y4)-(y1-y2)*(x3*y4-y3*x4)) / det)
+
+            if (inter_x >= 0 and inter_x <= 1920) and (inter_y >= 0 and inter_y <= 1080): 
+
+                if (inter_x, inter_y) not in pontos:
+                    pontos.append((inter_x, inter_y))
+
+                if theta < 1:
+                    pontos_dessa_linha.append((inter_x, inter_y))
+    
+    if theta < 1:
+        pontos_por_linhas.append(pontos_dessa_linha)
+
+
+for sublist in pontos_por_linhas:
+    sublist.sort(key=lambda x: x[0])
+pontos_por_linhas.sort(key=lambda x: x[0][0])
+
+casas = []
+for i in range(len(pontos_por_linhas)-1):
+    for j in range(len(pontos_por_linhas[i])-1):
+        pontos = [pontos_por_linhas[i][j], pontos_por_linhas[i][j+1], pontos_por_linhas[i+1][j], pontos_por_linhas[i+1][j+1]]
+        casas.append(pontos)
+
+# for i in casas:
+#     imagem_pontos = imagem.copy()
+#     for j in i:
+        
+#         x, y = j
+#         cv.circle(imagem_pontos, (x, y), 5, (255, 0, 0), 2)
+
+#     cv.imshow('Intersecções', imagem_pontos)
+#     cv.waitKey(0)
+#     cv.destroyAllWindows()
+
+for i in range(64):
+    ponto1 = casas[i][0] # Topo esquerdo
+    ponto2 = casas[i][1] # Topo direito
+    ponto3 = casas[i][3] # Base direita
+    ponto4 = casas[i][2] # Base esquerda
+
+    pontos = [ponto1, ponto2, ponto3, ponto4]
+
+    coordenadas_x = [p[0] for p in pontos]
+    coordenadas_y = [p[1] for p in pontos]
+
+    x_min = min(coordenadas_x)
+    x_max = max(coordenadas_x)
+    y_min = min(coordenadas_y)
+    y_max = max(coordenadas_y)
+
+    imagem_recortada = imagem[y_min-120:y_max, x_min:x_max]
+
+    # Exibir a imagem recortada
+    cv.imshow('Imagem Recortada', imagem_recortada)
+    cv.waitKey(0)
+    cv.destroyAllWindows()
+
+# imagem_pontos = imagem.copy()
+# for ponto in pontos:
+#     x, y = ponto
+#     cv.circle(imagem_pontos, (x, y), 5, (255, 0, 0), 2)
+
+# cv.imshow('Intersecções', imagem_pontos)
+# cv.waitKey(0)
+# cv.destroyAllWindows()
+
+# for pontos_linha in pontos_por_linhas:
+#     imagen_linha = imagem.copy()
+#     for ponto in pontos_linha:
+#         x, y = ponto
+#         cv.circle(imagen_linha, (x, y), 5, (255, 0, 0), 2)
+
+        # cv.imshow('Intersecções', imagen_linha)
+        # cv.waitKey(0)
+        # cv.destroyAllWindows()
+
+
